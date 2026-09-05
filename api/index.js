684e6d13775dc8d2929e4febf9163dcd1fa20d10
Rejuyanul-Haque-Rifat <rejuyanulhaquerifat814343@gmail.com>
2026-09-06 01:07:38 +0600

    V1

--- api/index.js
+++ api/index.js
@@ -21,7 +21,7 @@ if (!admin.apps.length) {
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       privateKey: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n').replace(/"/g, '') : undefined,
     }),
-    databaseURL: "https://unity-floor-north-hostel-bpi-default-rtdb.firebaseio.com"
+    databaseURL: "https://unity-floor-north-hostel-bpi-default-rtdb.asia-southeast1.firebasedatabase.app"
   });
 }
 
